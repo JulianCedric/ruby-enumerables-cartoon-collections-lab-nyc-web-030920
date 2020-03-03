104364d1954cbd48127afba474b4785fc33b0ce2
Julian Pormentilla <julian.pormentilla@gmail.com>
2020-03-03 16:45:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,12 +11,10 @@ planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 
 def summon_captain_planet(array)
   array.map! do |elem| 
-    elem.capitalize
+    elem.capitalize + "!"
   end 
 array 
 end
-
-p summon_captain_planet(planeteer_calls)
 
 def long_planeteer_calls# code an argument here
   # Your code here
