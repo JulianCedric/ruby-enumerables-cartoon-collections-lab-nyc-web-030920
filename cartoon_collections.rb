b27a435a531868dfdf538862c722d845ba6f965a
Julian Pormentilla <julian.pormentilla@gmail.com>
2020-03-03 16:55:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,6 @@ def long_planeteer_calls(array)
   array.any? { |elem| elem.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
