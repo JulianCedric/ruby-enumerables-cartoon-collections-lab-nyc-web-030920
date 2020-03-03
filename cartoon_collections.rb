c120a890213c38e930847728e8aaa79462bee33d
Julian Pormentilla <julian.pormentilla@gmail.com>
2020-03-03 16:55:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,6 +23,6 @@ def long_planeteer_calls(array)
   array.any? { |elem| elem.length > 4 }
 end
 
-def find_the_cheese
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
