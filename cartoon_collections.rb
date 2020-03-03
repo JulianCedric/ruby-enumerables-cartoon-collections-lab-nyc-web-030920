a964bbdc422e81c39a9dd6ba09a02002f7c03827
Julian Pormentilla <julian.pormentilla@gmail.com>
2020-03-03 17:12:48 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,43 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+def roll_call_dwarves(array)
+  array.each_with_index do |elem, i| 
+    puts "#{i+1} #{elem}"
+  end
+array
+end 
+
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def summon_captain_planet(array)
+  array.map! do |elem| 
+    elem.capitalize + "!"
+  end 
+array 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+short_words = ["puff", "go", "two"]
+assorted_words = ["two", "go", "industrious", "bop"]
+
+def long_planeteer_calls(array)
+  array.any? { |elem| elem.length > 4 }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+snacks = ["crackers", "gouda", "thyme"]
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  new = []
+  i = 0 
+  while i < cheese_types.length do 
+    if array.include?(cheese_types[i]) 
+      new << cheese_types[i]
+    end 
+    i += 1 
+  end 
+if new == nil  
+  return nil
+end 
+new[0]  
 end
