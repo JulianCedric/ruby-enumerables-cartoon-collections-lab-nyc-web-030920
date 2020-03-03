5da22a99b5588c1256d32160cd07304472f01bc9
Julian Pormentilla <julian.pormentilla@gmail.com>
2020-03-03 16:46:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,10 @@ def summon_captain_planet(array)
 array 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+
+
+def long_planeteer_calls
 end
 
 def find_the_cheese# code an argument here
